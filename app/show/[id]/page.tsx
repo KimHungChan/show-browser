3c836ff3443671f8df3ef3fc1bc6fb3b7f1849ab
KimHung <michaeltkhchan@gmail.com>
2024-07-01 01:03:00 +0100

    Fix some styling and populate show page

--- app/show/[id]/page.tsx
+++ app/show/[id]/page.tsx
@@ -3,12 +3,13 @@
 import useShowStore from '@/store/store';
 import { useParams } from 'next/navigation';
 import { Show } from '@/types/types';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 const ShowPage = () => {
   const selectedShow = useShowStore((state) => state.selectedShow);
   const getShowById = useShowStore((state) => state.getShowById);
   const setSelectedShow = useShowStore((state) => state.setSelectedShow);
+  const [showMore, setShowMore] = useState(false);
   const { id } = useParams();
 
   useEffect(() => {
@@ -27,10 +28,50 @@ const ShowPage = () => {
   }, [selectedShow, id, getShowById, setSelectedShow]);
 
   return (
-    <div>
-      <h1>{selectedShow?.name}</h1>
-      <img src={selectedShow?.image?.original} alt={selectedShow?.name} />
-      <p>{selectedShow?.summary}</p>
+    <div className="text-white flex flex-col items-center sm:items-baseline px-4 sm:px-16">
+      <h1 className="text-5xl">{selectedShow?.name}</h1>
+      <div className="flex justify-between my-4 w-full flex-wrap">
+        <div>
+          <p>Language: {selectedShow?.language}</p>
+          <p>Premiered: {selectedShow?.premiered}</p>
+          {selectedShow?.status === 'Ended' && (
+            <p>Ended: {selectedShow?.ended}</p>
+          )}
+          <p>Runtime: {selectedShow?.runtime} minutes</p>
+          {/* if show ended show the end date */}
+        </div>
+        <div className="sm:text-right">
+          {' '}
+          {selectedShow?.rating?.average && (
+            <p>Rating: {selectedShow?.rating?.average}</p>
+          )}
+          <p>Genres: {selectedShow?.genres.join(' • ')}</p>
+        </div>
+      </div>
+      <img
+        // src={selectedShow?.image?.original}
+        srcSet={`${selectedShow?.image?.medium} 700w, ${selectedShow?.image?.original} 1200w`}
+        alt={selectedShow?.name}
+        className="h-96 w-64 object-cover"
+      />
+      <p className="mt-8">
+        {showMore
+          ? selectedShow?.summary.replace(/<[^>]*>?/gm, '')
+          : selectedShow?.summary
+              .replace(/<[^>]*>?/gm, '')
+              .slice(0, 200)
+              .padEnd(203, '...')}
+      </p>
+      <button
+        onClick={() => setShowMore(!showMore)}
+        className="text-white bg-black p-2 rounded-md  flex w-full justify-center mt-4 bg-opacity-50 hover:bg-opacity-70 bg-black-gray"
+      >
+        {!showMore ? (
+          <i className="fa-solid fa-chevron-down"></i>
+        ) : (
+          <i className="fa-solid fa-angle-up"></i>
+        )}
+      </button>
     </div>
   );
 };
--- components/SearchBar/SearchBar.tsx
+++ components/SearchBar/SearchBar.tsx
@@ -1,10 +1,20 @@
 'use client';
 import { useState } from 'react';
 import useShowStore from '@/store/store';
+import { usePathname, useRouter } from 'next/navigation';
 
 const SearchBar = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const searchTvShows = useShowStore((state) => state.searchTvShows);
+  const pathname = usePathname();
+  const router = useRouter();
+
+  const OnSearch = (searchTerm: string) => {
+    searchTvShows(searchTerm);
+    if (pathname !== '/') {
+      router?.push('/');
+    }
+  };
 
   return (
     <div className="flex border bg-black text-white">
@@ -13,15 +23,18 @@ const SearchBar = () => {
         placeholder="Search for TV Shows..."
         value={searchTerm}
         className="w-48 outline-none bg-black text-white p-2"
-        onChange={(value) => setSearchTerm(value.target.value)}
+        onChange={(value) => {
+          setSearchTerm(value.target.value);
+          if (pathname === '/') OnSearch(value.target.value);
+        }}
         onKeyDown={(event) => {
           if (event.key === 'Enter') {
-            searchTvShows(searchTerm);
+            OnSearch(searchTerm);
           }
         }}
       />
       <button
-        onClick={() => searchTvShows(searchTerm)}
+        onClick={() => OnSearch(searchTerm)}
         disabled={searchTerm.length === 0}
         className={'p-2'}
       >
